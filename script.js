c469a41b4a0a5fd85d1cde4004086ebe957cdc1d
Mauricio Lopez <weleven9@gmail.com>
2025-10-22 22:36:17 -0500

    Part2_basd

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log('Hi!');
-console.log('Hello!');
+let calle = 'calle 1'
+
+console.log(calle);
+console.log(Calle);
+
+// Regla 1: No se puede usar palabras clave reservadas
+// Regla 2: No puede iniciar con número
+// Regla 3: Tiene que tener significado
+// Regla 4: No puede tener espacios ni guiones
+// Regla 5: Sensible a mayúsculas
+
+let street;
+let phone;
